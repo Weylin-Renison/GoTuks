929d1cbff280e05f7b380d376d95c30756b1859a
wprenison <wprenison@gmail.com>
2016-07-11 19:44:10 +0200

    -Welcome Screen layout -gif library and bg -added some assets

---
+++ app/src/main/java/com/gometro/gotuks/ActivityIntro.java
@@ -0,0 +1,17 @@
+package com.gometro.gotuks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wprenison on 2016/07/11.
+ */
+public class ActivityIntro extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
